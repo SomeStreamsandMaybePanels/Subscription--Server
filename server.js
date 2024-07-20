07db7e04fbc5a01d9bc6b4fa00b1e0ee0c0f5c86
SSAMP <deldays@proton.me>
2024-07-20 16:02:04 -0400

    Added a function to parse and print incoming notifications

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const xml2js = require('xml2js')
 const { query } = require('./mongodb')
 const { subscribeToChannel } = require('./subscribe')
 
@@ -9,8 +10,6 @@ const port = process.env.PORT
 const HUB_URL = process.env.HUB_URL
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 // Endpoint to handle subscription verification (GET request)
 app.get('/websub', (req, res) => {
@@ -24,9 +23,21 @@ app.get('/websub', (req, res) => {
     }
 })
 
+app.use(bodyParser.text({type: 'application/atom+xml'}))
 // Endpoint to handle notifications (POST request)
 app.post('/websub', (req, res) => {
     console.log('Notification received:', req.body);
+    // Parse the XML data
+    xml2js.parseString(req.body, { explicitArray: false }, (err, result) => {
+        if (err) {
+            console.error('Failed to parse XML:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        // Print the parsed XML data in a human-readable format
+        console.log('Parsed XML data:', JSON.stringify(result, null, 2));
+    });
     res.status(200).send('OK');
 })
 
