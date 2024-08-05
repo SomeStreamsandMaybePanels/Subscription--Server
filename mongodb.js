d514b01334d858a4d89354566273b042476be510
Your Name <deldays@proton.me>
2024-08-05 12:35:06 -0400

    Revised mongodb.js module file

--- mongodb.js
+++ mongodb.js
@@ -1,34 +1,16 @@
-
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri = 'mongodb://rob/'
 
-
-
-/**
- * @description function to connect the mongoDB to the MongoDB database
- * @return {Promise}
- */
-async function query() {
-  mongoDB = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-      useUnifiedTopology: true
-    }
-  })
-  
+module.exports = async (mongoClient, mongoDatabaseName, mongoDatabaseSubscriptionCollectionName) => {
   const queryObj = { "notifications": true }
-  
+
   // Connect the mongoDB to the server (optional starting in v4.7)
 
-  await mongoDB.connect()
-  await mongoDB.db('admin').command({ ping: 1 })
+  await mongoClient.connect()
+  await mongoClient.db('admin').command({ ping: 1 })
   // ping to confirm successful connection
   console.log('Pinged your deployment. You successfully connected to MongoDB!')
   try {
-    const myCollection = await mongoDB.db(process.env.MONGODB_NAME).collection(process.env.MONGODB_SUBSCRIPTION_CONFIG_COLLECTION)
+    const myCollection = await mongoClient.db(mongoDatabaseName).collection(mongoDatabaseSubscriptionCollectionName)
     const query = await myCollection.find(queryObj)
     const allValues = await query.toArray()
     console.log(allValues)
@@ -36,10 +18,8 @@ async function query() {
     return allValues
   } catch (error) {
     console.error(error)
-  
+
   }
 }
 
-
-
-module.exports = { query }
+// todo revise this so that it will be dependent on an injected instance of MongoDBClient
