55b793af5557f386d370ddf71ca40ae0b551c4c9
Your Name <deldays@proton.me>
2024-08-08 15:42:56 -0400

    edited dependency paths

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const express = require('express')
-const logger = require('./logger')
+const logger = require('./utilities/logger')
 const bodyParser = require('body-parser')
 const xml2js = require('xml2js')
-const { query } = require('./mongodb')
-const { subscribeToChannel } = require('./subscribe')
+const { query } = require('./utilities/mongodb')
+const { subscribeToChannel } = require('./commands/subscribe')
 
 const app = express()
 const port = process.env.NOTIFICATION_SERVER_PORT
 const HUB_URL = process.env.NOTIFICATION_HUB_URL
-
+require('./utilities/websocket')(app)
 app.use(bodyParser.text({ type: 'application/atom+xml' }))
 
 // Endpoint to handle subscription verification (GET request)
@@ -36,6 +36,13 @@ app.post('/websub', (req, res) => {
     } else {
       logger.info('forwarding data to parent program')
       process.send({ data: result })
+
+      // Broadcast the data to all Websocket clients
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(result))
+        }
+      })
       res.status(200).send('OK')
     }
   })
@@ -45,7 +52,7 @@ app.listen(port, () => {
   logger.info(`Server is running on port ${port}`)
 })
 
-async function main () {
+async function main() {
   const TOPICS = await query()
   TOPICS.forEach(topic => subscribeToChannel(topic.youtubeChannelID, HUB_URL))
 }
